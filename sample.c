2961225d2569b9784bdf73f4b7ea21d88ffa7736
Bradley Rastrullo <11504992+brastrullo@users.noreply.github.com>
2024-06-24 05:19:39 -0400

    add main revised

--- sample.c
+++ sample.c
@@ -4,24 +4,29 @@
 
 #define BUFSIZE 256
 
-int read_file(const char *filename) {
-    FILE* ptr = fopen(filename, "r");
+int main(int argc, char** argv) {
+    char *fileName = argv[2];
+    // Check for invalid sequences in the user input
+    if (strstr(fileName , "..") || strchr(fileName , '/') || strchr(fileName , '\\')) {
+        printf("Invalid filename.\n");
+        return 1;
+    }
 
-    if (ptr == NULL) {
+    char fileBuffer[BUFSIZE];
+    snprintf(fileBuffer, sizeof(fileBuffer), "/home/user/files/%s", fileName);
+    // GOOD: We know that the filename is safe and stays within the public folder
+    FILE *file = fopen(fileBuffer, "wb+");
+  
+    if (file == NULL) {
         printf("File cannot be opened.\n");
         return(-1);
     }
 
     char buff[BUFSIZE];
 
-    while (fgets(buff, BUFSIZE, ptr) != NULL) {
+    while (fgets(buff, BUFSIZE, file) != NULL) {
         printf("%s", buff);
     }
 
     return 0;
-}
-
-int main(int argc, char *argv[]) {
-  read_file(argv[1]);
-  return 0;
 }
