3b45b44ddd3d71a3e00e156e84625bea6890e629
Bradley Rastrullo <11504992+brastrullo@users.noreply.github.com>
2024-06-24 04:49:05 -0400

    add main

--- sample.c
+++ sample.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>    
+#include <string.h>
 
 #define BUFSIZE 256
-                                
+
 int read_file(const char *filename) {
     FILE* ptr = fopen(filename, "r");
-                                
+
     if (ptr == NULL) {
         printf("File cannot be opened.\n");
         return(-1);
     }
-                                
+
     char buff[BUFSIZE];
-                                
+
     while (fgets(buff, BUFSIZE, ptr) != NULL) {
         printf("%s", buff);
     }
-        
-    return 0; 
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Usage: %s <filename>\n", argv[0]);
+    return -1;
+  }
+
+  if (read_file(argv[1]) == -1) {
+    printf("Failed to read file '%s'\n", argv[1]);
+    return -1;
+  }
+
+  return 0;
 }
